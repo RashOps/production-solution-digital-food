57e83d6fc448e395c5be21628003bf134525e5e3
Rayhan Seh Fred Touboui <touboui.sehfredrayhan@gmail.com>
2026-04-11 20:39:53 +0200

    Add script to retrieve and display food information with CSV export

---
+++ get_food.py
@@ -0,0 +1,16 @@
+from food import Food
+import argparse
+
+import sys
+print("Running script...")
+
+parser = argparse.ArgumentParser("Food Informations")
+parser.add_argument('-f', '--food', help="your food name", default='tomate')
+
+args = parser.parse_args()
+
+food = Food()
+food.retrieve_food_infos(args.food)
+food.display_food_infos()
+food.save_to_csv_file("food.csv")
+print(f"Aliment gras ({food.get_name()}) ? : {food.is_fat()}")
